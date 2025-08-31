125200ca7242ad07727344c22f1203bcdc14a41b
Tsai Li Ming <1571401+tsailiming@users.noreply.github.com>
2025-08-31 16:14:31 +0800

    Script to deploy model

--- scripts/serve-model.sh
+++ scripts/serve-model.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
 
+BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+NAMESPACE=demo
+export NAME=$1 #qwen2.5-vl-7b-instruct
+export MODEL_PATH=$2 #Qwen/Qwen2.5-VL-7B-Instruct/
+
+if [ -z "$NAME" ] || [ -z "$MODEL_PATH" ]; then
+  echo "Usage: $0 <name> <model_path>"
+  echo "Example: $0 qwen2.5-vl-7b-instruct Qwen/Qwen2.5-VL-7B-Instruct/"
+  exit 1
+fi
+
+
+oc delete isvc/$NAME -n ${NAMESPACE}
+oc delete servingruntime/$NAME -n ${NAMESPACE}
+
+envsubst '$NAME, $MODEL_PATH' \
+    < ${BASE}/yaml/demo/sr.yaml.tmpl | oc apply -n ${NAMESPACE} -f -
+
+envsubst '$NAME, $MODEL_PATH' \
+    < ${BASE}/yaml/demo/isvc.yaml.tmpl | oc apply -n ${NAMESPACE} -f -
